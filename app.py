3d8dc795d4eab5bef2d6339632a49bdb3a842e07
soulfulmusic <soulfulmusic968@gmail.com>
2026-01-31 17:44:33 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Statistics Learning App", layout="wide")
+
+
+st.title("Statistics Learning App")
+st.markdown("### Learn Statistics with Manual Calculation + Python Code")
+
+
+st.success("Select a topic from the sidebar")
